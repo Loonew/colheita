d861007de7a2d2f18ae2266879c66814e72a0040
Loonie <joabprojecao@gmail.com>
2020-11-23 03:16:07 -0300

    tpl site

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function() {//se a url for nada alem do .com, chama o page.php
+//nisso, dentro do tpl, já tem o header, o index e o fotter prontos. Juntou os 3
     
 	$page = new Page();
 
